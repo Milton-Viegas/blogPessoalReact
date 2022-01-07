9009f500a205b39632269b65c70e72ecc028a676
Milton-Viegas <milton.viegas.br@gmail.com>
2022-01-07 10:06:33 +0000

    modified home, login

--- src/components/estaticos/footer/Footer.tsx
+++ src/components/estaticos/footer/Footer.tsx
@@ -20,7 +20,7 @@ function Footer() {
         <Grid alignItems="center" item xs={12}>
             <Box className='box1'>
                 <Box paddingTop={1} display="flex" alignItems="center" justifyContent="center">
-                    <Typography variant="h5" align="center" gutterBottom className='textos'>Siga-nos nas redes sociais </Typography>
+                    <Typography variant="h5" align="center" gutterBottom className='textos'>Siga-me nas redes sociais </Typography>
                 </Box>
                 <Box display="flex" alignItems="center" justifyContent="center">
                     <a href="https://www.facebook.com/milton.viegas.7" target="_blank" rel='noreferrer'>
@@ -36,12 +36,11 @@ function Footer() {
             </Box>
             <Box className='box2'>
                 <Box paddingTop={1}>
-                    <Typography variant="subtitle2" align="center" gutterBottom className='textos' >© 2021 Copyright:</Typography>
-                </Box>
-                <Box>
-                    <a href="https://github.com/Milton-Viegas" target="_blank" rel='noreferrer'>
-                        <Typography variant="subtitle2" gutterBottom className='textos' align="center">Milton Viegas</Typography>
-                    </a>
+                    <Typography variant="subtitle2" align="center" gutterBottom className='textos' >© 2021 Copyright: </Typography>
+                    <Typography variant="subtitle2" align="center" gutterBottom className='textos' >
+                        <a href="https://github.com/Milton-Viegas" target="_blank" rel='noreferrer'> Milton Viegas</a></Typography> 
+
+                    
                 </Box>
             </Box>
         </Grid>
